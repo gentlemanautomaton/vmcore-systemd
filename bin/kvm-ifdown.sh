7dca7eab39e0e0bd79f8f436e39689229e8bb297
Dusty Wilson <dusty@linux.com>
2015-06-17 16:36:21 -0700

    Added firewalling feature

--- bin/kvm-ifdown.sh
+++ bin/kvm-ifdown.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-date >> /tmp/kvm-ifdown.$$.log
-echo $* >> /tmp/kvm-ifdown.$$.log
-env >> /tmp/kvm-ifdown.$$.log
+if [ -n "$nic" ]; then
+	# remove nic from the bridge
+	/usr/bin/env brctl delif $bridge $nic
+
+	# detach chain for this nic from firewall
+	/usr/bin/env iptables -D INPUT -i $nic -j $nic
+	/usr/bin/env iptables -D OUTPUT -o $nic -j $nic
+	/usr/bin/env iptables -D FORWARD -i $nic -j $nic
+	/usr/bin/env iptables -D FORWARD -o $nic -j $nic
+
+	# remove firewall chain for this nic
+	/usr/bin/env iptables -F $nic
+	/usr/bin/env iptables -X $nic
+
+	# turn down the nic
+	/usr/bin/env ip link set $nic down
+fi
--- bin/kvm-ifup.sh
+++ bin/kvm-ifup.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-date >> /tmp/kvm-ifup.$$.log
-echo $* >> /tmp/kvm-ifup.$$.log
-env >> /tmp/kvm-ifup.$$.log
+if [ -n "$nic" ]; then
+	# turn up the nic
+	/usr/bin/env ip link set $nic up
+
+	# setup firewall chain for this nic
+	/usr/bin/env iptables -N $nic
+	/usr/bin/env iptables -F $nic
+
+	# restrict to IP if defined
+	if [ -n "$ip" ]; then
+		/usr/bin/env iptables -A $nic -i $nic ! -s $ip -j REJECT
+		/usr/bin/env iptables -A $nic -o $nic ! -d $ip -j REJECT
+	fi
+
+	# load custom firewall if exists
+	if [ -n "$firewall" -a -x "$firewall" ]; then
+		. "$firewall"
+	fi
+
+	# attach chain for this nic to firewall
+	/usr/bin/env iptables -A INPUT -i $nic -j $nic
+	/usr/bin/env iptables -A OUTPUT -o $nic -j $nic
+	/usr/bin/env iptables -A FORWARD -i $nic -j $nic
+	/usr/bin/env iptables -A FORWARD -o $nic -j $nic
+
+	# add nic to the bridge
+	/usr/bin/env brctl addif $bridge $nic
+fi
