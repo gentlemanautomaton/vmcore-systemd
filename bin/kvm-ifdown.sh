6d00d90c099c717fed13a2d0b337eb0c4c5f50eb
Dusty Wilson <dusty@linux.com>
2015-07-20 22:04:03 -0700

    Added -w flag to iptables commands

--- bin/kvm-ifdown.sh
+++ bin/kvm-ifdown.sh
@@ -5,14 +5,14 @@ if [ -n "$nic" ]; then
 	/usr/bin/env brctl delif $bridge $nic
 
 	# detach chain for this nic from firewall
-        /usr/bin/env iptables -D INPUT -m physdev --physdev-is-bridged --physdev-in $nic -j $nic
-        /usr/bin/env iptables -D OUTPUT -m physdev --physdev-is-bridged --physdev-out $nic -j $nic
-        /usr/bin/env iptables -D FORWARD -m physdev --physdev-is-bridged --physdev-in $nic -j $nic
-        /usr/bin/env iptables -D FORWARD -m physdev --physdev-is-bridged --physdev-out $nic -j $nic
+        /usr/bin/env iptables -w -D INPUT -m physdev --physdev-is-bridged --physdev-in $nic -j $nic
+        /usr/bin/env iptables -w -D OUTPUT -m physdev --physdev-is-bridged --physdev-out $nic -j $nic
+        /usr/bin/env iptables -w -D FORWARD -m physdev --physdev-is-bridged --physdev-in $nic -j $nic
+        /usr/bin/env iptables -w -D FORWARD -m physdev --physdev-is-bridged --physdev-out $nic -j $nic
 
 	# remove firewall chain for this nic
-	/usr/bin/env iptables -F $nic
-	/usr/bin/env iptables -X $nic
+	/usr/bin/env iptables -w -F $nic
+	/usr/bin/env iptables -w -X $nic
 
 	# turn down the nic
 	/usr/bin/env ip link set $nic down
--- bin/kvm-ifup.sh
+++ bin/kvm-ifup.sh
@@ -8,18 +8,18 @@ if [ -n "$nic" ]; then
 	/usr/bin/env ip link set $nic up
 
 	# setup firewall chain for this nic
-	/usr/bin/env iptables -N $nic
-	/usr/bin/env iptables -F $nic
+	/usr/bin/env iptables -w -N $nic
+	/usr/bin/env iptables -w -F $nic
 
 	# restrict to IP if defined
 	if [ -n "$ip" ]; then
 		OLDIFS="$IFS";IFS=" ,"
 		for X in $ip; do
-			/usr/bin/env iptables -A $nic -m physdev --physdev-is-bridged --physdev-in $nic -s $X -j RETURN
-			/usr/bin/env iptables -A $nic -m physdev --physdev-is-bridged --physdev-out $nic -d $X -j RETURN
+			/usr/bin/env iptables -w -A $nic -m physdev --physdev-is-bridged --physdev-in $nic -s $X -j RETURN
+			/usr/bin/env iptables -w -A $nic -m physdev --physdev-is-bridged --physdev-out $nic -d $X -j RETURN
 		done
 		IFS="$OLDIFS"
-		/usr/bin/env iptables -A $nic -j REJECT
+		/usr/bin/env iptables -w -A $nic -j REJECT
 	fi
 
 	# load custom firewall if exists
@@ -28,10 +28,10 @@ if [ -n "$nic" ]; then
 	fi
 
 	# attach chain for this nic to firewall
-	/usr/bin/env iptables -A INPUT -m physdev --physdev-is-bridged --physdev-in $nic -j $nic
-	/usr/bin/env iptables -A OUTPUT -m physdev --physdev-is-bridged --physdev-out $nic -j $nic
-	/usr/bin/env iptables -A FORWARD -m physdev --physdev-is-bridged --physdev-in $nic -j $nic
-	/usr/bin/env iptables -A FORWARD -m physdev --physdev-is-bridged --physdev-out $nic -j $nic
+	/usr/bin/env iptables -w -A INPUT -m physdev --physdev-is-bridged --physdev-in $nic -j $nic
+	/usr/bin/env iptables -w -A OUTPUT -m physdev --physdev-is-bridged --physdev-out $nic -j $nic
+	/usr/bin/env iptables -w -A FORWARD -m physdev --physdev-is-bridged --physdev-in $nic -j $nic
+	/usr/bin/env iptables -w -A FORWARD -m physdev --physdev-is-bridged --physdev-out $nic -j $nic
 
 	# add nic to the bridge
 	/usr/bin/env brctl addif $bridge $nic
