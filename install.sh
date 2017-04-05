1a6f24544ae4a6523c609bcc5b89c072ea579d2c
Joshua Sjoding <ga@josh.hey.nu>
2017-04-05 15:46:30 -0700

    Support use of OVMF as EFI firmware

--- install.sh
+++ install.sh
@@ -3,12 +3,16 @@
 # Everything in this file should be done via package management, not this script.  FIXME
 
 # requires...
-apt-get install qemu-kvm bridge-utils crudini arp-scan nmap
+apt-get install qemu-kvm bridge-utils ovmf crudini arp-scan nmap
 
 # install the unit template
 mkdir -p /usr/lib/systemd/system
 cp -v /srv/vmcore/systemd/vmcore-kvm@.service /usr/lib/systemd/system/
 systemctl daemon-reload
+
+# install the ovmf symlink
+mkdir -p /srv/vmcore/ovmf
+ln -sv /usr/share/ovmf/OVMF.fd /srv/vmcore/ovmf/bios.bin
 
 # install the vmcore symlink
 ln -sv /srv/vmcore/bin/vmcore /usr/bin/vmcore
